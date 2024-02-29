8ea3acf10cd649017595b5d75ce5314f1588b134
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-02-29 16:38:41 +0200

    add navigation to app

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+function PageNotFound() {
+  return (
+    <h1>
+      <PageNav />
+      <h1>404</h1>
+    </h1>
+  );
+}
+
+export default PageNotFound;
